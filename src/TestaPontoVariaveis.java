10707a6972449adc524d90efc8bb89be8dd827ea
pkovbin <reginaazzi@hotmail.com>
2022-02-20 09:55:29 -0300

    fisrt commit

---
+++ src/TestaPontoVariaveis.java
@@ -0,0 +1,20 @@
+
+public class TestaPontoVariaveis {
+public static void main (String[]args) {
+	
+	double salario = 1250.70;
+	System.out.println(" O meu salario é " + salario);
+
+
+
+	double divisao = 5.0/2;
+	System.out.println (divisao);
+	
+	
+	
+	
+	
+	
+}
+
+}
---
+++ src/TestaVariaveis.java
@@ -0,0 +1,25 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		char letra = ('a');
+		System.out.println(letra);
+		
+		char valor = 65;
+		System.out.println(valor);
+		valor= (char) (valor +1);
+		System.out.println(valor);
+		
+		String palavra = "alura curso online de tecnologia";
+		
+		System.out.println(palavra);
+		
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+		
+		
+		
+		
+	}
+
+}
